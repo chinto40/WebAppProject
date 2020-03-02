5b6fdf6774b85485af7f9e560b1d2e6a894c89aa
ldill06 <lndill91@gmail.com>
2020-03-02 14:22:03 -0600

    Designed the user dashboard screen to include calories left for the day, current weight, goal weight, current calories logged, and goal calories per day. There is also the ability to log weight or calories by clicking a button on the user dashboard, which displays a modal that includes the date and weight or calories. Restructured some of the files in components folder.

--- Node/WorkOutWebApp/workout/src/components/workout_builder.js
+++ Node/WorkOutWebApp/workout/src/components/workout_builder.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container } from "@material-ui/core";
-import MenuBar from "../menu_bar/menu_bar";
+import MenuBar from "./menu_bar";
 
 export default function WorkoutBuilder() {
   return (
